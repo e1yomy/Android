b1c2f22c6011b35ae36820e3b87336e5d3358f95
elyo_ <elyo_my@hotmail.com>
2017-04-04 17:03:43 -0600

    no message

--- TracKids/app/src/main/java/elyo/my/trackids/Principal.java
+++ TracKids/app/src/main/java/elyo/my/trackids/Principal.java
@@ -1,10 +1,13 @@
 package elyo.my.trackids;
 
+
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,7 +17,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class Principal extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, Mapa.OnFragmentInteractionListener{
+
+    FragmentManager manager=getSupportFragmentManager();
+    android.support.v4.app.FragmentTransaction transaction= manager.beginTransaction();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +30,6 @@ public class Principal extends AppCompatActivity
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -76,13 +75,44 @@ public class Principal extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
 
-        if (id == R.id.nav_inicio) {
-            // Handle the camera action
+
+        int id = item.getItemId();
+        switch (id)
+        {
+            case R.id.nav_inicio:
+                transaction.add(R.id.contenedor,new Mapa());
+                break;
+            case R.id.nav_hijos:
+
+                break;
+            case R.id.nav_salir:
+
+                break;
+            case R.id.nav_clavepadre:
+
+                break;
+            case R.id.nav_clavehijo:
+
+                break;
+            case R.id.nav_agregarpadre:
+
+                break;
+            case R.id.nav_agregarhijo:
+
+                break;
         }
+        transaction.commit();
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
