2a54e42941ec936e78d65a17030f747f7e1b7db3
elyo_ <elyo_my@hotmail.com>
2017-04-17 12:01:15 -0600

    ITAI avance

---
+++ TransparenciaDigital/app/src/main/java/transparencia/itai/com/transparenciadigital/Splash.java
@@ -0,0 +1,30 @@
+package transparencia.itai.com.transparenciadigital;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //finish();
+            }},1500);
+    }
+}
