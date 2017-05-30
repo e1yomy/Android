07c7b7220b2f7f38f718502925cf97f6af802399
e1yomy <elyo_my@hotmail.com>
2017-05-30 09:12:26 -0600

    Revert "TracKids: Prospecto de entrega de proyecto para Isaac"
    This reverts commit 19fa39a167d814a88ebe64cbb796108f33d101d9.

--- TracKids/app/src/main/java/elyo/my/trackids/IngresarClaves.java
+++ TracKids/app/src/main/java/elyo/my/trackids/IngresarClaves.java
@@ -11,6 +11,8 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 
 import static elyo.my.trackids.Principal.AgregarHijo;
+import static elyo.my.trackids.Principal.PantallaHijos;
+import static elyo.my.trackids.Principal.preferences;
 
 
  /**
@@ -135,7 +137,9 @@ public class IngresarClaves extends Fragment {
                  try {
                      AgregarHijo(editUsuario.getText().toString(), editPin.getText().toString());
                      Thread.sleep(500);
-
+                     if (preferences.getBoolean("existe", false)) {
+                         PantallaHijos();
+                     }
                  } catch (Exception ex) {
                      String s=ex.getMessage();
                  }
